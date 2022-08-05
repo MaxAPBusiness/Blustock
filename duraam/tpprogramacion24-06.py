cca4f4bbc0bf397ce754f6b8568b9afede647e79
julipa <lucianoalvarez@iico.edu.ar>
2022-08-04 21:45:25 -0300

    god

---
+++ duraam/tpprogramacion24-06.py
@@ -0,0 +1,14 @@
+tipo_de_entrada=["a", "adultos", "m", "menores", "j", "jubilados"]
+empleado=["001", "juan", "002", "emanuel", "003", "francisco"]
+ventas=["23/06/2021", "001", "a", "3", 200, "0", "no",666,"23/02/2020", "002", "a", "2", 200, "0", "no",667,"23/02/2020", "002", "a", "6", 400, "0", "no",668]
+i=0
+#ventas del dia de un empleado especifico
+ventas_de_empleado=0
+for i in ventas(len(ventas)):
+	ventas[i+=1] == "001"
+	
+
+
+
+
+print(ventas_de_empleado)
