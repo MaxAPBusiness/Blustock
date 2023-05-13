d039c4838b54bcaa5a358b7e954037bde566a71b
Maximo Alejandro Pellegrino <mpellegrino004@gmail.com>
2023-05-13 11:54:27 -0300

    Cambié algunas cosas del menú y conecté el menú a las pantallas.
    Cambié la forma de hacer las pantallas para que ocuparan menos líneas de código. Cambié los nombres de las actions del menú para que fueran más fáciles de usar en el código y cambié su tamaño en el ui para que fueran legibles. Añadí opciones y cambié algunos nombres para que fueran más adecuados. Por último, conecté el menú a las pantallas. Todavía queda agrandar el tamaño de la letra de algunas partes de la interfaz porque cuando es muy chiquita no se lee,

--- blustock/main.py
+++ blustock/main.py
@@ -7,38 +7,62 @@ from db.bbdd import BBDD
 bbdd=BBDD()
 bbdd.refrescarBBDD()
 
-"""class PantallaAlumnos(QtWidgets.QWidget):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi(os.path.join(os.path.abspath(os.getcwd()), f'uis{os.sep}penegordo.ui'), self)"""
-
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
-
         uic.loadUi(os.path.join(os.path.abspath(os.getcwd()), f'uis{os.sep}main.ui'), self)
-        """pantallaAlumnos=PantallaAlumnos()
-        self.stackedWidget.addWidget(pantallaAlumnos)"""
+
+        pantallaAlumnos=QtWidgets.QWidget()
+        uic.loadUi(os.path.join(os.path.abspath(os.getcwd()), f'uis{os.sep}alumnos.ui'), pantallaAlumnos)
+        pantallaGrupos=QtWidgets.QWidget()
+        uic.loadUi(os.path.join(os.path.abspath(os.getcwd()), f'uis{os.sep}grupos.ui'), pantallaGrupos)
+        pantallaHerramientas=QtWidgets.QWidget()
+        uic.loadUi(os.path.join(os.path.abspath(os.getcwd()), f'uis{os.sep}herramientas.ui'), pantallaHerramientas)
+        pantallaHistorial=QtWidgets.QWidget()
+        uic.loadUi(os.path.join(os.path.abspath(os.getcwd()), f'uis{os.sep}historial.ui'), pantallaHistorial)
+        pantallaMovimientos=QtWidgets.QWidget()
+        uic.loadUi(os.path.join(os.path.abspath(os.getcwd()), f'uis{os.sep}movimientos.ui'), pantallaMovimientos)
+        pantallaOtroPersonal=QtWidgets.QWidget()
+        uic.loadUi(os.path.join(os.path.abspath(os.getcwd()), f'uis{os.sep}otro_personal.ui'), pantallaOtroPersonal)
+        pantallaSubgrupos=QtWidgets.QWidget()
+        uic.loadUi(os.path.join(os.path.abspath(os.getcwd()), f'uis{os.sep}subgrupos.ui'), pantallaSubgrupos)
+        pantallaTurnos=QtWidgets.QWidget()
+        uic.loadUi(os.path.join(os.path.abspath(os.getcwd()), f'uis{os.sep}turnos.ui'), pantallaTurnos)
+        pantallaUsuarios=QtWidgets.QWidget()
+        uic.loadUi(os.path.join(os.path.abspath(os.getcwd()), f'uis{os.sep}usuarios.ui'), pantallaUsuarios)
+        pantallas=[pantallaAlumnos, pantallaGrupos, pantallaHerramientas, pantallaHistorial,
+                   pantallaMovimientos, pantallaOtroPersonal, pantallaSubgrupos, pantallaTurnos,
+                   pantallaUsuarios]
+        for pantalla in pantallas:
+            self.stackedWidget.addWidget(pantalla)
+        
+        self.opcionStock.triggered.connect(lambda: self.stackedWidget.setCurrentIndex(2))
+        self.opcionSubgrupos.triggered.connect(lambda: self.stackedWidget.setCurrentIndex(6))
+        self.opcionGrupos.triggered.connect(lambda: self.stackedWidget.setCurrentIndex(1))
+        self.opcionAlumnos.triggered.connect(lambda: self.stackedWidget.setCurrentIndex(0))
+        self.opcionOtroPersonal.triggered.connect(lambda: self.stackedWidget.setCurrentIndex(5))
+        self.opcionTurnos.triggered.connect(lambda: self.stackedWidget.setCurrentIndex(7))
+        self.opcionMovimientos.triggered.connect(lambda: self.stackedWidget.setCurrentIndex(4))
+        self.opcionUsuarios.triggered.connect(lambda: self.stackedWidget.setCurrentIndex(8))
+        self.opcionHistorial.triggered.connect(lambda: self.stackedWidget.setCurrentIndex(3))
 
         with open(os.path.join(os.path.abspath(os.getcwd()), 'styles.qss'), 'r') as file:
             self.setStyleSheet(file.read())
-        self.stackedWidget.setCurrentIndex(0)
+        self.stackedWidget.setCurrentIndex(4)
         self.show()
 
-    """def login(self):
-        db.cur.execute("SELECT count(*) FROM personal WHERE usuario = ?",(self.usuariosLineEdit.text(),))
-        check = db.cur.fetchall()
+    def login(self):
+        bbdd.cur.execute("SELECT count(*) FROM personal WHERE usuario = ?",(self.usuariosLineEdit.text(),))
+        check = bbdd.cur.fetchall()
         if check == 1:
-            db.cur.execute("SELECT count(*) FROM personal WHERE usuario = ? and contrasena = ?",(self.usuariosLineEdit.text(),self.passwordLineEdit.text(),))
-            check = db.cur.fetchall()
+            bbdd.cur.execute("SELECT count(*) FROM personal WHERE usuario = ? and contrasena = ?",(self.usuariosLineEdit.text(),self.passwordLineEdit.text(),))
+            check = bbdd.cur.fetchall()
             if check == 1:
                 self.stackedWidget.setCurrentIndex(1)
-        self.show()"""
 
 app=QtWidgets.QApplication(sys.argv)
 
-for fuente in os.listdir(
-    os.path.join(os.path.abspath(os.getcwd()), f'rsc{os.sep}fonts')):
+for fuente in os.listdir(os.path.join(os.path.abspath(os.getcwd()), f'rsc{os.sep}fonts')):
     QtGui.QFontDatabase.addApplicationFont(
         os.path.join(os.path.abspath(os.getcwd()),
             f'rsc{os.sep}fonts{os.sep}{fuente}')
