44dc661b1f28a319c1662b803b11c55466f414f5
Santiago Piñero <89141124+s-anti@users.noreply.github.com>
2023-06-16 22:27:26 -0300

    Las ids y descripciones estaban raro, revisar

--- blustock/dal/queries/alumnos.sql
+++ blustock/dal/queries/alumnos.sql
@@ -1,6 +1,6 @@
 SELECT p.nombre_apellido, c.descripcion, p.dni 
 FROM personal p
-JOIN clases c ON c.id = p.id_clase
+JOIN clases c ON c.descripcion = p.id_clase
 WHERE c.descripcion IN (
     '1A', '1B', '1C', '2A', '2B', '2C', '3A', '3B', '3C', '4A', '4B', '4C',
     '5A', '5B', '6A', '6B', '6C', '7A', '7B', '7C', 'Egresado'
