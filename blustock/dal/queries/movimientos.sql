b070bc4e5855433f606400ee55ee04485600b48c
Santiago Piñero <89141124+s-anti@users.noreply.github.com>
2023-06-17 01:29:57 -0300

    Cambié el query un poco y regeneré la tabla mov

--- blustock/dal/queries/movimientos.sql
+++ blustock/dal/queries/movimientos.sql
@@ -1,12 +1,12 @@
 SELECT ti.descripcion, s.descripcion, e.descripcion, m.cant, p.nombre_apellido,
-m.fecha_hora, m.motivo, m.id_turno, u.descripcion
+m.fecha_hora, m.descripcion, m.id_turno, u.descripcion
 FROM movimientos m
 JOIN stock s ON s.id=m.id_elem
 JOIN estados e ON e.id =m.id_estado
 JOIN personal p ON p.dni = m.id_persona
 JOIN tipos_mov ti ON ti.id=m.id_tipo
 JOIN turnos tu ON tu.id=m.id_turno
-JOIN ubicaiones u ON u.id=t.id_ubi
+JOIN ubicaciones u ON u.id=tu.id_ubi
 WHERE m.id_turno LIKE ?
 OR s.descripcion LIKE ?
 OR e.descripcion LIKE ?
@@ -14,5 +14,5 @@ OR p.nombre_apellido LIKE ?
 OR m.fecha_hora LIKE ?
 OR m.cant LIKE ?
 OR ti.descripcion LIKE ?
-OR m.motivo LIKE ?
+OR m.descripcion LIKE ?
 OR u.descripcion LIKE ?;
