73c4334863038b20823b4227b550b695925f9567
Santiago Piñero <89141124+s-anti@users.noreply.github.com>
2023-06-19 01:57:10 -0300

    deleteTurnos (verifElimTurnos no está en el DAL?)

--- blustock/main.py
+++ blustock/main.py
@@ -1583,7 +1583,7 @@ class MainWindow(QtWidgets.QMainWindow):
             
 
             self.generarBotones(
-                None, None, tabla, rowNum) # NOne es saveTurno y deleteTurno
+                self.saveTurnos, self.deleteTurnos, tabla, rowNum) # NOne es saveTurno y deleteTurno
 
         # Método setRowHeight: cambia la altura de una fila.
         tabla.setRowHeight(0, 35)
@@ -1600,7 +1600,80 @@ class MainWindow(QtWidgets.QMainWindow):
         self.stackedWidget.setCurrentIndex(7)
         tabla.cellClicked.connect(
             lambda row: self.obtenerFilaEditada(tabla, row))
-     
+        
+    def saveTurnos(self):
+        pass
+
+    def deleteTurnos(self, datos: list) -> None:
+        """Este método elimina una fila de una tabla de la base de
+        datos
+        
+        Parámetros
+        ----------
+        idd: int | None = None
+            El número que relaciona la fila de la tabla de la UI con la
+            fila de la tabla de la base de datos.
+            Default: None
+        """
+        # Obtenemos la tabla a la que vamos a realizarle la eliminación
+        tabla=self.pantallaStock.tableWidget
+        # Obtenemos la fila que se va a eliminar.
+        row = (tabla.indexAt(self.sender().pos())).row()
+        if datos:
+            idd=None
+        else:
+            idd=datos[row][0]
+        # Si no se pasó el argumento idd, significa que la fila no está
+        # relacionada con la base de datos. Eso significa que la fila
+        # se insertó en la tabla de la UI, pero aún no se guardaron los
+        # cambios en la base de datos. En ese caso...
+        if not idd:
+            # ...solo debemos sacarla de la UI.
+            return tabla.removeRow(row)
+        # Si está relacionada con la base de datos, antes de eliminar,
+        # tenemos que verificar que la PK de la fila no
+        # tenga relaciones foráneas con otras tablas. Si llegase a
+        # tener, no podemos permitir una eliminación normal por dos
+        # motivos. El primero, necesitamos registrar todos los campos
+        # eliminados en el historial, y eliminar todo de una nos
+        # complica registrar que tablas se eliminaron. El segundo, si
+        # un profe se equivoca y elimina todo, no hay vuelta atrás.
+        # Para esta verificación, llamamos a la función del dal.
+        hayRelacion = dal.verifElimTurnos(idd)
+        if hayRelacion:
+            mensaje = """        El turno tiene movimientos o un
+            seguimiento de reparación relacionados. Por motivos de seguridad,
+            debe eliminar primero los registros relacionados antes de eliminar
+            este turno."""
+            return PopUp('Advertencia', mensaje).exec()
+        
+        # Si no está relacionado, pregunta al usuario si confirma
+        # eliminar la fila y le advierte que la acción no se puede
+        # deshacer.
+        mensaje = """        Esta acción no se puede deshacer.
+        ¿Desea eliminar el turno?"""
+        popup = PopUp("Pregunta", mensaje).exec()
+
+        # Si el usuario presionó el boton sí...
+        if popup == QtWidgets.QMessageBox.StandardButton.Yes:
+            # # Obtenemos los datos para guardarlos en el historial.
+            # desc = tabla.item(row, 0).text()
+            # cond = tabla.item(row, 1).text()
+            # rep = tabla.item(row, 2).text()
+            # baja = tabla.item(row, 3).text()
+            # grupo = tabla.item(row, 5).text()
+            # subgrupo = tabla.item(row, 6).text()
+            # ubi = tabla.item(row, 7).text()
+
+            # datosEliminados = f"desc: {desc}, ubi: {ubi},\n cant cond: {cond}, cant rep: {rep}, cant baja: {baja},\n grupo: {grupo}, subgrupo: {subgrupo}"
+
+            # # Insertamos los datos en el historial para que quede registro.
+            # dal.insertarHistorial(self.usuario, "eliminación", "stock", row, datosEliminados)
+            # # Eliminamos los datos
+            # dal.eliminarDatos(idd)
+            print("Me dijieron que no lo haga")
+        self.fetchTurnos()
+
     def fetchUsuarios(self):
         """Este método obtiene los datos de la tabla stock y los
         inserta en la tabla de la interfaz de usuario.
@@ -1635,7 +1708,7 @@ class MainWindow(QtWidgets.QMainWindow):
 
         
             self.generarBotones(
-                lambda: print("No implementado jaja"), lambda: print("No implementado jaja"), tabla, rowNum)
+                self.saveUsuarios, self.deleteUsuarios, tabla, rowNum)
 
         tabla.setRowHeight(0, 35)
         tabla.resizeColumnsToContents()
@@ -1649,6 +1722,9 @@ class MainWindow(QtWidgets.QMainWindow):
             6, QtWidgets.QHeaderView.ResizeMode.Stretch)
 
         self.stackedWidget.setCurrentIndex(8)
+
+    def saveUsuarios(self):
+        print("No implementado")
 
     def deleteUsuarios(self, datos: list) -> None:
         """Este método elimina una fila de una tabla de la base de
