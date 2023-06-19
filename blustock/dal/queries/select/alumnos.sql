6637d002a2fc7c6a47dfcce13cb46fb3d30385b4
Santiago Piñero <89141124+s-anti@users.noreply.github.com>
2023-06-19 01:38:36 -0300

    c.id, no c.descripcion igual en todos

--- blustock/dal/queries/select/alumnos.sql
+++ blustock/dal/queries/select/alumnos.sql
@@ -1,6 +1,6 @@
 SELECT p.nombre_apellido, c.descripcion, p.dni 
 FROM personal p
-JOIN clases c ON c.descripcion = p.id_clase
+JOIN clases c ON c.id = p.id_clase
 WHERE c.descripcion NOT IN ( 'profesor', 'egresado'
 ) AND (p.nombre_apellido LIKE ?
 OR c.descripcion LIKE ?
--- blustock/dal/queries/select/usuarios.sql
+++ blustock/dal/queries/select/usuarios.sql
@@ -1,6 +1,6 @@
 SELECT p.dni, p.nombre_apellido, c.descripcion, p.usuario, p.contrasena  
 FROM personal p
-JOIN clases c ON c.descripcion = p.id_clase
+JOIN clases c ON c.id = p.id_clase
 WHERE p.usuario IS NOT NULL
 AND (p.nombre_apellido LIKE ?
 OR p.usuario LIKE ?
